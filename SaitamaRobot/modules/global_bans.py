c73b984fd257cd338c5643f8385bed8eed6b9020
PAINBOI2008 <81083883+PAINBOI2008@users.noreply.github.com>
2021-05-10 14:19:07 +0530

    Update global_bans.py

--- SaitamaRobot/modules/global_bans.py
+++ SaitamaRobot/modules/global_bans.py
@@ -91,32 +91,32 @@ def gban(update: Update, context: CallbackContext):
 
     if int(user_id) in DEV_USERS:
         message.reply_text(
-            "That user is part of the Association\nI can't act against our own.",
+            "That user is part of the Organization\nLet Me Grab My Popcorn Pack There Is A Slight Chance That This Will Turn Into A Big War.",
         )
         return
 
     if int(user_id) in DRAGONS:
         message.reply_text(
-            "I spy, with my little eye... a disaster! Why are you guys turning on each other?",
+            "Hmm I Can't Do That Or It Will Make You Fall In Danger",
         )
         return
 
     if int(user_id) in DEMONS:
         message.reply_text(
-            "OOOH someone's trying to gban a Demon Disaster! *grabs popcorn*",
+            "Nope Not Gonna Do That To My Supporter.",
         )
         return
 
     if int(user_id) in TIGERS:
-        message.reply_text("That's a Tiger! They cannot be banned!")
+        message.reply_text("That's an Uchiha! He cannot be banned!")
         return
 
     if int(user_id) in WOLVES:
-        message.reply_text("That's a Wolf! They cannot be banned!")
+        message.reply_text("That's a Villager! He cannot be banned!")
         return
 
     if user_id == bot.id:
-        message.reply_text("You uhh...want me to punch myself?")
+        message.reply_text("You uhh...want me to punch myself sorry but Bonkhammer 404 Is Coming Here For You")
         return
 
     if user_id in [777000, 1087968824]:
